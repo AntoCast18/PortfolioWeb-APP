f6ee1de476acb81d0281d9a0b386057cff1fd2a3
AntoCast18 <101275905+AntoCast18@users.noreply.github.com>
2022-05-16 11:53:59 -0300

    nueva carpeta banck

---
+++ PortfolioWeb/BackEnd/src/main/java/com/portfolioanto/Anto/AntoApplication.java
@@ -0,0 +1,13 @@
+package com.portfolioanto.Anto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntoApplication.class, args);
+	}
+
+}
---
+++ PortfolioWeb/BackEnd/src/test/java/com/portfolioanto/Anto/AntoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolioanto.Anto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
